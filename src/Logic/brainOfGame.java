6b58438456837ea15ec0a3e37098f796cc18c018
Manish Sharma <omsharma571222@gmail.com>
2020-04-10 08:55:59 +0545

    For inner loop

--- src/Logic/brainOfGame.java
+++ src/Logic/brainOfGame.java
@@ -83,7 +83,9 @@ public class brainOfGame {
                 System.out.println("---------");
                 System.out.print("| ");
                 for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
 
+                    }
                 }
             }
         }
